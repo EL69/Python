f03653d1972340e3ea5a9d452360e7c499a8372b
Elekwa <Dark_Knight_007@hotmail.com>
2026-03-25 22:28:54 +0000

    git commit -m  'Input number logic'

--- Noughts-and-Crosses.py
+++ Noughts-and-Crosses.py
@@ -10,11 +10,35 @@ s7 = '7'
 s8 = '8'
 s9 = '9'
 
+while True:
 
-print(" _______________")
-print(f"|  {s1} | {s2}  | {s3}  |")
-print("|____|____|____|")
-print(f"|  {s4} |  {s5} |  {s6} |")
-print("|____|____|____|")
-print(f"|  {s7} |  {s8} |  {s9} |")
-print("|____|____|____|")
+    print(" _______________")
+    print(f"|  {s1} | {s2}  | {s3}  |")
+    print("|____|____|____|")
+    print(f"|  {s4} |  {s5} |  {s6} |")
+    print("|____|____|____|")
+    print(f"|  {s7} |  {s8} |  {s9} |")
+    print("|____|____|____|")
+    print()
+
+    move = input('What is your move: ')
+
+    if move == '1':
+        s1 == 'x'
+    if move == '2':
+        s2 == 'x'
+    if move == '3':
+        s3 == 'x'
+    if move == '4':
+        s4 == 'x'
+    if move == '5':
+        s5 == 'x'
+    if move == '6':
+        s6 == 'x'
+    if move == '7':
+        s7 == 'x'
+    if move == '8':
+        s8 == 'x'
+    if move == '9':
+        s9 == 'x'
+    
