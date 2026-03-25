c781123a3af0dbd3463ee9c598d61aa90d0e20ca
Elekwa <Dark_Knight_007@hotmail.com>
2026-03-25 20:07:53 +0000

    git commit -m 'Very Final Board'

--- Noughts-and-Crosses.py
+++ Noughts-and-Crosses.py
@@ -1,8 +1,20 @@
 # Project Noughts and Crosses
-print(' _______________')
-print('|    |    |    |')
-print('|____|____|____|')
-print('|    |    |    |')
-print('|____|____|____|')
-print('|    |    |    |')
-print('|____|____|____|')
+# square designations / variables
+s1 = '1'
+s2 = '2'
+s3 = '3'
+s4 = '4'
+s5 = '5'
+s6 = '6'
+s7 = '7'
+s8 = '8'
+s9 = '9'
+
+
+print(" _______________")
+print(f"|  {s1} | {s2}  | {s3}  |")
+print("|____|____|____|")
+print(f"|  {s4} |  {s5} |  {s6} |")
+print("|____|____|____|")
+print(f"|  {s7} |  {s8} |  {s9} |")
+print("|____|____|____|")
